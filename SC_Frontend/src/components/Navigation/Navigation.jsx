ed64f15b65da46566e0ff1f18dbfaf11a81a5bb6
martin0626 <scbsteel12@gmail.com>
2024-11-28 19:28:01 +0200

    Added Navigation

--- SC_Frontend/src/components/Navigation/Navigation.jsx
+++ SC_Frontend/src/components/Navigation/Navigation.jsx
@@ -1,35 +1,60 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FaAlignJustify } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Navigation(){
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+    const handleOpenMenu = ()=>{
+        setIsOpenMenu(!isOpenMenu);        
+    }
+
+
     return (
         <nav className="navigation">
-            <span className="navigation-logo">
-                <img src="https://t3.ftcdn.net/jpg/02/45/84/16/360_F_245841615_d7QzRv937jfiC176rmKK60ckNXU9V76z.jpg" alt="" />
-            </span>
-            <NavLink 
-                to='/'
-                className={({isActive}) =>
-                    isActive ? "active" : ""
+            <div className="navigation-logo">
+                <span>
+                    <img src="https://t3.ftcdn.net/jpg/02/45/84/16/360_F_245841615_d7QzRv937jfiC176rmKK60ckNXU9V76z.jpg" alt="" />
+                </span>
+            </div>
+            <div className="navigation-links">
+                <NavLink 
+                    to='/'
+                    className={({isActive}) =>
+                        isActive ? "active" : ""
                 } 
-            >
-                Home
-            </NavLink>
-            <NavLink 
-                to='stores'
-                className={({isActive}) =>
-                    isActive ? "active" : ""
+                >
+                    Home
+                </NavLink>
+                <NavLink 
+                    to='stores'
+                    className={({isActive}) =>
+                        isActive ? "active" : ""
                 } 
-            >
-                Stores
-            </NavLink>
-            <NavLink 
-                to='/'
-                className={({isActive}) =>
-                    isActive ? "active" : ""
+                >
+                    Stores
+                </NavLink>
+                
+                <FaAlignJustify onClick={handleOpenMenu} />
+            </div>
+            <div className={`navigation-menu ${isOpenMenu ? 'navigation-open' : 'navigation-closed'}`}>
+                <NavLink 
+                    to='stores'
+                    className={({isActive}) =>
+                        isActive ? "active-mob" : ""
                 } 
-            >
-                Login
-            </NavLink>
+                >
+                    Stores
+                </NavLink>
+                <NavLink 
+                    to='stores'
+                    className={({isActive}) =>
+                        isActive ? "active-mob" : ""
+                } 
+                >
+                    Stores
+                </NavLink>
+            </div>
         </nav>
     )
 }
