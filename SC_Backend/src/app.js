aa7e8c7390f054fc99f9a8aa6f102275c6e6bf99
martin0626 <scbsteel12@gmail.com>
2024-12-16 19:53:02 +0200

    Added More changes

--- SC_Backend/src/app.js
+++ SC_Backend/src/app.js
@@ -4,11 +4,14 @@ const app = express();
 const salonRouter  = require('./routes/salonRouters')
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const cors = require('cors')
 
 
 //Middlewares
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+app.use(cors());
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
