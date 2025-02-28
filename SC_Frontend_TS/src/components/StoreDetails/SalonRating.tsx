cc36961b5f84e783e79f5a27e373dd49f0d029ae
martin0626 <scbsteel12@gmail.com>
2025-02-28 18:59:45 +0200

    FE - Updated Slider to use React-Slick for better UI/UX

--- SC_Frontend_TS/src/components/StoreDetails/SalonRating.tsx
+++ SC_Frontend_TS/src/components/StoreDetails/SalonRating.tsx
@@ -1,7 +1,80 @@
 import UiSlider from "../UI/Slider";
+import {  FaStar } from "react-icons/fa";
+import RateStars from "../UI/RatingStars";
 
 export default function Rating({rating}: {rating: number}){
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+
     return (
-            <UiSlider/>
+            <div className="uiSlider">
+                <h2><FaStar  /> Reviews</h2>
+                <UiSlider settings={settings}>
+                    <article className="slide-card">
+                        <div className="card-content">
+                            <div className="card-content-profile">
+                                <RateStars rate={3}/>
+                                <div className="card-content-profile-img">
+                                    <img src="https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg" alt="" />
+                                </div>
+                            </div>
+                            <div className="card-content-text">
+                                <h4><span style={{color: '#ffffffa8', fontSize: '0.9rem'}}>Name:</span> John Smith</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nam numquam, explicabo est animi corrupti nobis impedit
+                                    fugiat necessitatibus quasi dolores iste non tempore error exercitationem tempora, sunt mollitia blanditiis! Lorem, ipsum 
+                                    dolor sit amet consectetur adipisicing elit. Saepe reprehenderit pariatur qui sapiente dolores, unde nesciunt dicta expe
+                                    dita neque architecto exercitationem laudantium cumque. Cumque enim odit ad, maiores sit molestias!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="slide-card">
+                        <div className="card-content">
+                            <div className="card-content-profile">
+                                <RateStars rate={3}/>
+                                <div className="card-content-profile-img">
+                                    <img src="https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg" alt="" />
+                                </div>
+                            </div>
+                            <div className="card-content-text">
+                                <h4><span style={{color: '#ffffffa8', fontSize: '0.9rem'}}>Name:</span> John Smith</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nam numquam, explicabo est animi corrupti nobis impedit
+                                    fugiat necessitatibus quasi dolores iste non tempore error exercitationem tempora, sunt mollitia blanditiis! Lorem, ipsum 
+                                    dolor sit amet consectetur adipisicing elit. Saepe reprehenderit pariatur qui sapiente dolores, unde nesciunt dicta expe
+                                    dita neque architecto exercitationem laudantium cumque. Cumque enim odit ad, maiores sit molestias!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="slide-card">
+                        <div className="card-content">
+                            <div className="card-content-profile">
+                                <RateStars rate={3}/>
+                                <div className="card-content-profile-img">
+                                    <img src="https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg" alt="" />
+                                </div>
+                            </div>
+                            <div className="card-content-text">
+                                <h4><span style={{color: '#ffffffa8', fontSize: '0.9rem'}}>Name:</span> John Smith</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nam numquam, explicabo est animi corrupti nobis impedit
+                                    fugiat necessitatibus quasi dolores iste non tempore error exercitationem tempora, sunt mollitia blanditiis! Lorem, ipsum 
+                                    dolor sit amet consectetur adipisicing elit. Saepe reprehenderit pariatur qui sapiente dolores, unde nesciunt dicta expe
+                                    dita neque architecto exercitationem laudantium cumque. Cumque enim odit ad, maiores sit molestias!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </UiSlider>
+            </div>
     )
 }
--- SC_Frontend_TS/src/pages/Home.tsx
+++ SC_Frontend_TS/src/pages/Home.tsx
@@ -1,5 +1,10 @@
 export default function HomePage(){
+
+    
+
     return (
-        <h1>Home </h1>
+        <>
+           <h1>Home</h1>
+        </>
     )
 }
